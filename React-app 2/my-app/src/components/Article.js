63b9f7e236415274925aecbb92920585e20c279e
Ilya <imistrafik@yandex.ru>
2019-06-11 09:42:47 +0300

    React-app-new-feature-1

--- React-app 2/my-app/src/components/Article.js
+++ React-app 2/my-app/src/components/Article.js
@@ -4,28 +4,64 @@ import PropTypes from 'prop-types' // у Article это react и prop-types
 class Article extends React.Component {
     state = {
         visible: false,
+        articleStory: this.props.data.text,
+        btnVisibleInput: false,
     };
     handleReadMoreClck = e => {
         e.preventDefault();
         this.setState({ visible: true })
     };
+    handleArticleStorySaveChanges = e => {
+        this.setState({articleStory: e.currentTarget.value})
+    };
+    handleBtnMakeVisibleInput = e => {
+        e.preventDefault();
+            this.setState({ btnVisibleInput: true });
+    };
+    handleBtnMakeVisiblePText = e => {
+        e.preventDefault();
+            this.setState({ btnVisibleInput: false });
+    };
     render() {
         const { author, text, bigText } = this.props.data;
-        const { visible } = this.state;
+        const { visible, btnVisibleInput, articleStory } = this.state;
         return (
             <div className="article">
-                <p className="news__author">{author}:</p>
-                <p className="news__text">{text}</p>
-                {!visible && (
-                    <a
-                        onClick={this.handleReadMoreClck}
-                        href="#readmore"
-                        className="news__readmore"
-                    >
-                        Подробнее
-                    </a>
-                )}
-                {visible && <p className="news__big-text">{bigText}</p>}
+                <div>
+                    <p className="news__author">{author}:</p>
+                    {!btnVisibleInput
+                        ?
+                        <p className="news__text">{articleStory}</p>
+                        :
+                        <input
+                            onChange={this.handleArticleStorySaveChanges}
+                            type="text"
+                            value={articleStory}
+                        />
+                    }
+                    {!visible && (
+                        <a
+                            onClick={this.handleReadMoreClck}
+                            href="#readmore"
+                            className="news__readmore"
+                        >
+                            Подробнее
+                        </a>
+                    )}
+                    {visible && <p className="news__big-text">{bigText}</p>}
+                </div>
+                <div className="article_btn_rewrite">
+                    {!btnVisibleInput &&
+                    < button
+                        onClick={this.handleBtnMakeVisibleInput}
+                        >Изменить</button>
+                    }
+                    {btnVisibleInput &&
+                        <button
+                            onClick={this.handleBtnMakeVisiblePText}
+                        >Сохранить</button>
+                    }
+                </div>
             </div>
         )
     }
